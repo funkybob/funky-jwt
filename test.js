7f41b72ae81f10dcf25fb611fc393f6f6c482197
Curtis Maloney <curtis@tinbrain.net>
2018-06-24 18:32:11 +1000

    Remove old API from test.js

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
-import {JWT} from "./verify.js"
-import {decode, verify} from "./verifier.js"
+import {decode, verify} from "./verify.js"
 
 
 const config = {
@@ -11,10 +10,6 @@ document.querySelector('button').addEventListener('click', ev => {
 
   let token = document.querySelector('textarea').value.trim();
   let target = document.querySelector('p')
-
-  // Old API
-  let old = new JWT(token, config)
-  old.isValid().then(result => target.innerHTML = target.innerHTML + result)
 
   let jwt = decode(token)
   verify(jwt, config).then(result => target.innerHTML = target.innerHTML + result)
