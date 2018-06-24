768dc7368ebc1f51f51678fa4bcc561102fb5f65
Curtis Maloney <curtis@tinbrain.net>
2018-06-24 18:12:20 +1000

    Import all into index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import {b64d, b64e, str2bytes, bytes2str} from "./util.js"
 import {importKey} from "./crypto.js"
 import {fetchKeys} from "./keys.js"
 import {JWT} from "./verify.js"
-import {verifier} from "./verifier.js"
+import {decode, verify} from "./verifier.js"
 
 export {
     b64d, b64e, str2bytes, bytes2str,
     importKey,
     fetchKeys,
     JWT,
-    verifier
+    decode, verify
 }
