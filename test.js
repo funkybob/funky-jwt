173d3840172bfdd5a12d41dadcce99eefb7e158d
Curtis Maloney <curtis@tinbrain.net>
2018-06-24 18:11:06 +1000

    Simplify test

--- test.js
+++ test.js
@@ -2,23 +2,22 @@ import {JWT} from "./verify.js"
 import {decode, verify} from "./verifier.js"
 
 
+const config = {
+  alg: 'HS256',
+  secret: 'qwertyuiopasdfghjklzxcvbnm123456'
+}
+
 document.querySelector('button').addEventListener('click', ev => {
 
   let token = document.querySelector('textarea').value.trim();
   let target = document.querySelector('p')
 
   // Old API
-  let old = new JWT(token, {
-    alg: 'HS256',
-    secret: 'qwertyuiopasdfghjklzxcvbnm123456'
-  })
+  let old = new JWT(token, config)
   old.isValid().then(result => target.innerHTML = target.innerHTML + result)
 
   let jwt = decode(token)
-  verify(jwt, {
-    alg: 'HS256',
-    secret: 'qwertyuiopasdfghjklzxcvbnm123456'
-  }).then(result => target.innerHTML = target.innerHTML + result)
+  verify(jwt, config).then(result => target.innerHTML = target.innerHTML + result)
 
 })
 
